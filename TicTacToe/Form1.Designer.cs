b1199a6369b50ce92482916082693f5132fb52ba
Raul Merelli <raul.merelli@hotmail.it>
2019-04-12 13:32:54 +0200

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nuovaPartitaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.griglia = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.griglia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuovaPartitaToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(230, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nuovaPartitaToolStripMenuItem1
+            // 
+            this.nuovaPartitaToolStripMenuItem1.BackColor = System.Drawing.Color.Black;
+            this.nuovaPartitaToolStripMenuItem1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.nuovaPartitaToolStripMenuItem1.Name = "nuovaPartitaToolStripMenuItem1";
+            this.nuovaPartitaToolStripMenuItem1.Size = new System.Drawing.Size(77, 20);
+            this.nuovaPartitaToolStripMenuItem1.Text = "New Game";
+            this.nuovaPartitaToolStripMenuItem1.Click += new System.EventHandler(this.nuovaPartitaToolStripMenuItem_Click);
+            // 
+            // griglia
+            // 
+            this.griglia.Image = global::TicTacToe.Properties.Resources.Grid;
+            this.griglia.Location = new System.Drawing.Point(0, 24);
+            this.griglia.Name = "griglia";
+            this.griglia.Size = new System.Drawing.Size(230, 230);
+            this.griglia.TabIndex = 1;
+            this.griglia.TabStop = false;
+            this.griglia.MouseUp += new System.Windows.Forms.MouseEventHandler(this.griglia_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 253);
+            this.Controls.Add(this.griglia);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.griglia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox griglia;
+        private System.Windows.Forms.ToolStripMenuItem nuovaPartitaToolStripMenuItem1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        int[,] matrice;
+        int conta = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void controlla()
+        {
+            string vincitore = "";
+            for (int y = 0; y < 3; y++)
+            {
+                if (matrice[0, y] == matrice[1, y] && matrice[1, y] == matrice[2, y] && matrice[0, y] != 0)
+                {
+                    vincitore = ((char)matrice[0, y]).ToString();
+                }
+            }
+            for (int x = 0; x < 3; x++)
+            {
+                if (matrice[x, 0] == matrice[x, 1] && matrice[x, 1] == matrice[x, 2] && matrice[x, 0] != 0)
+                {
+                    vincitore = ((char)matrice[x, 0]).ToString();
+                }
+            }
+            if (matrice[0, 0] == matrice[1, 1] && matrice[1, 1] == matrice[2, 2] && matrice[0, 0] != 0)
+            {
+                vincitore = ((char)matrice[0, 0]).ToString();
+            }
+            else if (matrice[0, 2] == matrice[1, 1] && matrice[2, 0] == matrice[0, 2] && matrice[0, 2] != 0)
+            {
+                vincitore = ((char)matrice[0, 2]).ToString();
+            }
+            if (vincitore != "")
+            {
+                if (vincitore == "O")
+                {
+                    vincitore = "Player 2 (O)";
+                }
+                else
+                {
+                    vincitore = "Player 1 (X)";
+                }
+                MessageBox.Show("The winner is " + vincitore + "!");
+                nuovaPartitaToolStripMenuItem_Click(new object(), EventArgs.Empty);
+            }
+            if (conta == 9 && vincitore == "")
+            {
+                MessageBox.Show("Draw!");
+                nuovaPartitaToolStripMenuItem_Click(new object(), EventArgs.Empty);
+            }
+        }
+
+        void click(int pos, int x, int y)
+        {
+            int y2 = 0, x2 = pos;
+            while (x2 > 3)
+            {
+                x2 -= 3;
+                y2++;
+            }
+            x2--;
+            using (var graphics = Graphics.FromImage(griglia.Image))
+            {
+                graphics.CompositingMode = CompositingMode.SourceOver;
+                graphics.DrawImage(griglia.Image, 0, 0);
+                if (matrice[x2, y2] != 'O' && matrice[x2, y2] != 'X')
+                {
+                    conta++;
+                    if (conta % 2 == 0)
+                    {
+                        matrice[x2, y2] = 'O';
+                        graphics.DrawImage(new Bitmap(Properties.Resources.Nought), x, y);
+                    }
+                    else
+                    {
+                        matrice[x2, y2] = 'X';
+                        graphics.DrawImage(new Bitmap(Properties.Resources.Cross), x, y);
+                    }
+                }
+            }
+            griglia.Refresh();
+            controlla();
+        }
+
+        private void griglia_MouseUp(object sender, MouseEventArgs e)
+        {
+            int xCoordinate = e.X;
+            int yCoordinate = e.Y;
+            if (yCoordinate < 70)
+            {
+                if (xCoordinate < 70)
+                {
+                    click(1, 0, 0);
+                }
+                else if (xCoordinate >= 80 && xCoordinate <= 150)
+                {
+                    click(2, 80, 0);
+                }
+                else if (xCoordinate >= 160)
+                {
+                    click(3, 160, 0);
+                }
+            }
+            else if (yCoordinate >= 80 && yCoordinate <= 150)
+            {
+                if (xCoordinate < 70)
+                {
+                    click(4, 0, 80);
+                }
+                else if (xCoordinate >= 80 && xCoordinate <= 150)
+                {
+                    click(5, 80, 80);
+                }
+                else if (xCoordinate >= 160)
+                {
+                    click(6, 160, 80);
+                }
+            }
+            else if (yCoordinate >= 160)
+            {
+                if (xCoordinate < 70)
+                {
+                    click(7, 0, 160);
+                }
+                else if (xCoordinate >= 80 && xCoordinate <= 150)
+                {
+                    click(8, 80, 160);
+                }
+                else if (xCoordinate >= 160)
+                {
+                    click(9, 160, 160);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            matrice = new int[3, 3];
+        }
+
+        private void nuovaPartitaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            conta = 0;
+            griglia.Image = Properties.Resources.Grid;
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    matrice[x, y] = 0;
+                }
+            }
+        }
+    }
+}
